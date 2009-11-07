88eea464fc5741a063e3c39dc2c1db7850efeb1d
skarg <skarg@82c34705-1510-0410-8784-a13ad10d654b>
2009-11-07 15:14:23 +0000

    updated release script.

--- release.sh
+++ release.sh
@@ -2,12 +2,12 @@
 # Release helper for this project
 
 PROJECT=bacpypes
-CHANGELOG=ChangeLog
+CHANGELOG=ChangeLog-$1
 FRS_URL=skarg,bacpypes@frs.sourceforge.net:/home/frs/project/b/ba/bacpypes
 
 if [ -z "$1" ]
 then
-  echo "Usage: `basename $0` 0.0.0"
+  echo "Usage: `basename $0` datecode"
   echo "Creates the ChangeLog."
   echo "Creates the release files."
   echo "Tags the current version in subversion."
@@ -15,11 +15,9 @@ then
   exit 1
 fi
 
-# convert 0.0.0 to 0-0-0
-DOTTED_VERSION="$1"
-DASHED_VERSION="$(echo "$1" | sed 's/[\.*]/-/g')"
+VERSION=$1
 
-echo "Creating the release files for version ${DOTTED_VERSION}"
+echo "Creating the release files for version ${VERSION}"
 
 echo "Creating the ${PROJECT} change log..."
 rm ${CHANGELOG}
@@ -32,23 +30,22 @@ else
 echo "${CHANGELOG} created."
 fi
 
-ARCHIVE_NAME=${PROJECT}-${DOTTED_VERSION}
-TAGGED_NAME=${PROJECT}-${DASHED_VERSION}
+VERSION_NAME=${PROJECT}-${VERSION}
 SVN_BASE_URL=https://${PROJECT}.svn.sourceforge.net/svnroot/${PROJECT}
 
 SVN_TRUNK_NAME=${SVN_BASE_URL}/trunk
-SVN_TAGGED_NAME=${SVN_BASE_URL}/tags/${TAGGED_NAME}
-echo "Tagging the trunk as ${TAGGED_NAME}"
-svn copy ${SVN_TRUNK_NAME} ${SVN_TAGGED_NAME} -m "Created version ${ARCHIVE_NAME}" 
+SVN_TAGGED_NAME=${SVN_BASE_URL}/tags/${VERSION_NAME}
+echo "Tagging the trunk as ${VERSION_NAME}"
+svn copy ${SVN_TRUNK_NAME} ${SVN_TAGGED_NAME} -m "Created version ${VERSION_NAME}" 
 echo "done."
 
 echo "Getting a clean version out of subversion for Linux gzip"
-svn export ${SVN_TAGGED_NAME} ${ARCHIVE_NAME}
+svn export ${SVN_TAGGED_NAME} ${VERSION_NAME}
 echo "done."
 
-GZIP_FILENAME=${ARCHIVE_NAME}.tgz
+GZIP_FILENAME=${VERSION_NAME}.tgz
 echo "tar and gzip the clean directory"
-tar -cvvzf ${GZIP_FILENAME} ${ARCHIVE_NAME}/
+tar -cvvzf ${GZIP_FILENAME} ${VERSION_NAME}/
 echo "done."
 
 if [ -z "${GZIP_FILENAME}" ]
@@ -58,14 +55,14 @@ else
 echo "${GZIP_FILENAME} created."
 fi
 
-rm -rf ${ARCHIVE_NAME}
+rm -rf ${VERSION_NAME}
 
 echo "Getting another clean version out of subversion for Windows zip"
-svn export --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME}
-ZIP_FILENAME=${ARCHIVE_NAME}.zip
+svn export --native-eol CRLF ${SVN_TAGGED_NAME} ${VERSION_NAME}
+ZIP_FILENAME=${VERSION_NAME}.zip
 echo "done."
 echo "Zipping the directory exported for Windows."
-zip -r ${ZIP_FILENAME} ${ARCHIVE_NAME}
+zip -r ${ZIP_FILENAME} ${VERSION_NAME}
 
 if [ -z "${ZIP_FILENAME}" ]
 then
@@ -74,14 +71,14 @@ else
 echo "${ZIP_FILENAME} created."
 fi
 
-rm -rf ${ARCHIVE_NAME}
+rm -rf ${VERSION_NAME}
 
 echo "Sending to SourceForge..."
 
-mkdir ${ARCHIVE_NAME}
-mv ${ZIP_FILENAME} ${ARCHIVE_NAME}
-mv ${GZIP_FILENAME} ${ARCHIVE_NAME}
-mv ${CHANGELOG} ${ARCHIVE_NAME}
-scp -r ${ARCHIVE_NAME} ${FRS_URL}
+mkdir ${VERSION_NAME}
+mv ${ZIP_FILENAME} ${VERSION_NAME}
+mv ${GZIP_FILENAME} ${VERSION_NAME}
+mv ${CHANGELOG} ${VERSION_NAME}
+scp -r ${VERSION_NAME} ${FRS_URL}
 
 echo "Complete!"
