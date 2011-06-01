91353406cc834d38ede3e5946516d83420a3fbfc
joelbender <joelbender@82c34705-1510-0410-8784-a13ad10d654b>
2011-06-01 14:13:20 +0000

    improve setup to include the long description and classifiers, bump the rev

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
+import os
 from setuptools import setup
 
+#
+#   read
+#
+
+def read(fname):
+    """Utility function to read the contents of the README.txt file."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+#
+#   __main__
+#
+
 setup(name='BACpypes',
-      version='0.5b2',
-      description='BACnet Python Library',
-      author='Joel Bender',
-      author_email='joel@carrickbender.com',
-      url='http://bacpypes.sourceforge.net/',
-      packages=['bacpypes']
-      )
+    version='0.5b3',
+    description='BACnet Python Library',
+    author='Joel Bender',
+    author_email='joel@carrickbender.com',
+    url='http://bacpypes.sourceforge.net/',
+    packages=['bacpypes']
+    long_description=read('README.txt'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Networking",
+        "Topic :: Utilities",
+        ],
+    )
+
