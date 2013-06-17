bf539a0aeb4ce3e2713f784b9899be5c555dfb47
joelbender <joelbender@82c34705-1510-0410-8784-a13ad10d654b>
2013-06-17 15:29:10 +0000

    bump the version before releasing

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 #
 
 setup(name='BACpypes',
-    version='0.6.14',
+    version='0.7',
     description='BACnet Python Library',
     author='Joel Bender',
     author_email='joel@carrickbender.com',
