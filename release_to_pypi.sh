dc135741064cab2febb9867ab1a1d197dc14bade
joelbender <joelbender@82c34705-1510-0410-8784-a13ad10d654b>
2015-02-26 04:09:11 +0000

    updated to use twine to upload after building both an egg and a wheel

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# python setup.py bdist_egg upload
-sudo python -c "import setuptools; execfile('setup.py')" bdist_egg upload
+sudo python setup.py bdist_egg
+sudo python setup.py bdist_wheel
+twine upload dist/*
+
